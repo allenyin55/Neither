46a7dd637d537fa8ef08159532184f4c79863e87
Allen Yin <yinhaonan55@gmail.com>
2016-08-30 09:35:32 -0700

    first commit

---
+++ app/src/main/java/com/allen/neither/OnTopicActivity.java
@@ -0,0 +1,13 @@
+package com.allen.neither;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OnTopicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_topic);
+    }
+}
